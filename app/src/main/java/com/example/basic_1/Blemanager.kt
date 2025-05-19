b3cec10430584b40328cbece855a6b30151fb65c
graph <design1.keetronics@gmail.com>
2025-05-19 10:17:10 +0530

    Initial commit

--- app/src/main/java/com/example/basic_1/Blemanager.kt
+++ app/src/main/java/com/example/basic_1/Blemanager.kt
@@ -3,29 +3,22 @@ package com.example.basic_1
 import android.bluetooth.*
 import android.content.Context
 import java.util.*
-
-
-
 import android.util.Log
-
 class BleManager(private val context: Context) {
-
     companion object {
 //        const val ESP32_MAC_ADDRESS = "08:B6:1F:28:B4:6E" // MAC address for jig1 CC14
         const val ESP32_MAC_ADDRESS = "08:B6:1F:29:33:2E"// MAC address for jig2 CC14
+
         val SERVICE_UUID: UUID = UUID.fromString("4fafc201-1fb5-459e-8fcc-c5c9c331914b")
         val CHARACTERISTIC_UUID: UUID = UUID.fromString("beb5483e-36e1-4688-b7f5-ea07361b26a8")
         val CCCD_UUID: UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
     }
-
     private var bluetoothGatt: BluetoothGatt? = null
     private var bleCharacteristic: BluetoothGattCharacteristic? = null
     private var onMessageReceived: ((String) -> Unit)? = null
-
     fun setOnMessageReceivedListener(listener: (String) -> Unit) {
         onMessageReceived = listener
     }
-
     fun connect(device: BluetoothDevice, onConnected: () -> Unit) {
         bluetoothGatt = device.connectGatt(context, false, object : BluetoothGattCallback() {
 
